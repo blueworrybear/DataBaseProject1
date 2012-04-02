0bba3e00d3181dfe2c9b9860b5d49d5cc1171f4e
bear <blueworrybear@gmail.com>
2012-04-02 14:52:56 +0800

    first commit

---
+++ src/databaseproject1/DataBaseProject1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseproject1;
+
+/**
+ *
+ * @author bear
+ */
+public class DataBaseProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
